6d9678c81ebca85cb203af79d759348bebfc1176
Christopher Meeks <meekschris103@gmail.com>
2025-04-28 22:36:52 -0400

    Added CreateStudyGroupForm class to forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, EqualTo, Length
+from wtforms import StringField, PasswordField, SubmitField, TextAreaField, SelectField, BooleanField
+from wtforms.validators import DataRequired, EqualTo, Length, Email, ValidationError
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
@@ -9,6 +9,18 @@ class LoginForm(FlaskForm):
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+    def validate_email(self, email):
+        if not email.data.endswith('@southernct.edu'):
+            raise ValidationError('Please use your SCSU email (@southernct.edu)')
+        
+class CreateStudyGroupForm(FlaskForm):
+    title = StringField('Group Title', validators=[DataRequired(), Length(max=100)])
+    subject = StringField('Subject/Course', validators=[DataRequired(), Length(max=100)])
+    tags = StringField('Tags (comma separated)', validators=[Length(max=200)])
+    session = StringField('Preferred Meeting Time', validators=[Length(max=100)])
+    submit = SubmitField('Create Group')
